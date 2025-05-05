ff95e8e2d8cd7617db949eb75323a137abffcb9f
Ankit Shukla <134011141+Ankitshukla6121@users.noreply.github.com>
2025-05-06 00:41:04 +0530

    Update environment.prod.ts

--- Frontend/thekedarhub/src/environments/environment.prod.ts
+++ Frontend/thekedarhub/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true, // Set to true for production
-    apiUrl: 'https://your-production-api-url.com/api/users' // Backend API URL for production
+    apiUrl: 'https://https://thekedar-backend-1.onrender.com/api/users' // Backend API URL for production
   };
   
